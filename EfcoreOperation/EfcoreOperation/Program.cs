4ac11624fae2ac9d1991fa2fad077ce11a399598
Saurabh Kumar <Saurabhc519@gmail.com>
2024-04-24 15:41:57 +0530

    Seting Up Connection Strings

--- EfcoreOperation/EfcoreOperation/Program.cs
+++ EfcoreOperation/EfcoreOperation/Program.cs
@@ -1,11 +1,17 @@
+using EfcoreOperation.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbConnectionString")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
 
 var app = builder.Build();
 
--- EfcoreOperation/EfcoreOperation/obj/Debug/net8.0/EfcoreOperation.AssemblyInfo.cs
+++ EfcoreOperation/EfcoreOperation/obj/Debug/net8.0/EfcoreOperation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EfcoreOperation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2bcea5606a91c9620420f70a4e052328f540fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975c1b1d8a0f682ff56434b29af5a89e54cb42fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EfcoreOperation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EfcoreOperation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
